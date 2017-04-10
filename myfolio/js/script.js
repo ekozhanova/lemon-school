0bf17154e1e315d379810a6287d60d707c97f070
ekozhanova <lena.avadon@gmail.com>
2017-04-10 22:53:20 +0300

    Fix mobile menu js

--- myfolio/js/script.js
+++ myfolio/js/script.js
@@ -5,5 +5,13 @@ $(document).ready(function(){
     }, 500);
   });
 
-  $('#main-menu').slicknav();
+  $('#main-menu').slicknav({
+    init: function() {
+      $("ul.slicknav_nav li a").click(function(){
+        $('html, body').animate({
+          scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+      });
+    }
+  });
 });
